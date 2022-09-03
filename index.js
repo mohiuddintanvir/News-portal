c7a1eb87fe4aa8ba7de5b22000bf52ed35fa4301
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2022-09-04 02:34:17 +0600

    rewrite

--- index.js
+++ index.js
@@ -46,10 +46,14 @@ const entertainmentelement = fasionnews => {
                       </div>
                       <div class="col-md-8">
                         <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                          { <button onclick="othertdetails('${fasionpage._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"> More</button>}
+                          <h5 class="card-title">${fasionpage.title}</h5>
+                          <p class="card-text">${fasionpage.details}</p>
+                          <div class="d-flex">
+                          
+                          </div>
+
+                          
+                           <button onclick="othertdetails('${fasionpage._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"> More</button>
                         </div>
                       </div>
                     </div>
@@ -85,6 +89,12 @@ const showmodel = details => {
     `
 
 }
+
+
+
+
+
+
 
 
 
